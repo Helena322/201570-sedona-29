aa8a382ee1283b53489fd039720f29c543456c97
Helena <Helena322@yandex.ru>
2020-10-14 23:08:28 +0400

    Исправлена часть замечаний

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let button = document.querySelector('.search-heading-button');
 let layer = document.querySelector('.layer');
+
 button.onclick = function() {
 	layer.classList.toggle('modal-close');
 	layer.classList.toggle('modal-show');
