147efbbc163a4fd2a2cce4a51426ca53496aa96e
Helena <Helena322@yandex.ru>
2020-10-21 22:51:27 +0400

    Исправлены замечания по JS

--- js/script.js
+++ js/script.js
@@ -2,53 +2,59 @@ let button = document.querySelector('.search-heading-button');
 let layer = document.querySelector('.layer');
 let form = document.querySelector('.appointment-form');
 
-let arrival = document.querySelector('.arrival-input');
-let leaving = document.querySelector('.leaving-input');
-let adults = document.querySelector('.adults-input');
-let children = document.querySelector('.children-input');
+let arrival = form.querySelector('.arrival-input');
+let leaving = form.querySelector('.leaving-input');
+let adults = form.querySelector('.adults-input');
+let children = form.querySelector('.children-input');
 
 let isStorageSupport = true;
 let storage = "";
+let storage2 = "";
 
 try {
 	storage = localStorage.getItem("adults");
+	storage2 = localStorage.getItem("children");
 } 	catch (err) {
 	isStorageSupport = false;
 }
 
 button.addEventListener("click", function (evt) {
 	evt.preventDefault();
-	if (layer != ('modal-show')) {
-		layer.classList.toggle('modal-close');
+	if (layer.classList.contains("modal-close")) {
+		layer.classList.remove('modal-close');
+		layer.classList.add('modal-show');
+		arrival.focus();
+	} else {
+		layer.classList.remove('modal-show');
+		layer.classList.add('modal-close');
 	}
 
 	if (storage) {
 		adults.value = storage;
-		children.value = storage;
-	}
-
-	arrival.focus();
-});
-
-button.addEventListener("click", function (evt) {
-	evt.preventDefault();
-	if (layer != ('modal-close')) {
-		layer.classList.toggle('modal-show');
+		children.value = storage2;
 	}
 });
 
 form.addEventListener("submit", function (evt) {
 	if (!arrival.value || !leaving.value || !adults.value || !children.value) {
 		evt.preventDefault();
+		form.classList.remove("modal-error");
+		form.offsetWidth = form.offsetWidth;
 		form.classList.add("modal-error");
 	} else {
-		form.classList.remove("modal-error");
-	}
-
-	if (isStorageSupport) {
 		localStorage.setItem("adults", adults.value);
 		localStorage.setItem("children", children.value);
 	}
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (layer.classList.contains("modal-show")) {
+      evt.preventDefault();
+      layer.classList.remove("modal-show");
+      layer.classList.add('modal-close');
+    }
+  }
 });
 
 /*button.onclick = function() {
